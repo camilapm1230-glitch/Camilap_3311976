9b0ba2c1cec1fdbd40990f6944cffbe88f9736ea
CamiloQZ <camiloqzsena@gmail.com>
2026-04-13 10:05:34 -0500

    fix: puerto dinámico Railway

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
-import { PORT } from "./config.js";
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT);
-console.log(`Server on port http://localhost:3000`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
